c032110d53158ce426277b8f4464be870cbe5f92
nmogulla <nmogulla@unomaha.edu>
2022-02-27 21:24:38 -0600

    Added user account model

---
+++ useraccounts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django_countries.fields import CountryField
+
+
+class User(models.Model):
+    email = models.EmailField('email address', unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    about = models.TextField('about', max_length=500, blank=True)
+    # Delivery details
+    country = CountryField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    postcode = models.CharField(max_length=12, blank=True)
+    address_line_1 = models.CharField(max_length=150, blank=True)
+    address_line_2 = models.CharField(max_length=150, blank=True)
+    town_city = models.CharField(max_length=150, blank=True)
+    # User Status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
