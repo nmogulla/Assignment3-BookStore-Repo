6c170b9eeb9204f8019405953fe92dbe84ebe296
Navannetha <nmogulla@unomaha.edu>
2022-04-19 18:16:02 -0500

    Added account code

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+=2!f@xzy@4lswi17doy!pf^2br9x+i%m*z&gyf55d&+mo=^sg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
