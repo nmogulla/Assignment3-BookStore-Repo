d6684eb842a1a35c9ecce157929914d3a5dc33ae
Navannetha <nmogulla@unomaha.edu>
2022-04-20 23:35:16 -0500

    Added account code

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+
 admin.autodiscover()
 
 urlpatterns = [
@@ -29,7 +30,9 @@ urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account'
-                                                                                            ':password_change_done')), name='password_change'),
+                                                                                            ':password_change_done')),    name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
 
 if settings.DEBUG:
