bc06f8b3c2bbe940daadd088abe4556cf0112ead
Navannetha <nmogulla@unomaha.edu>
2022-04-20 23:36:37 -0500

    Added account code

--- core/urls.py
+++ core/urls.py
@@ -32,6 +32,10 @@ urlpatterns = [
     path('password-change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account'
                                                                                             ':password_change_done')),    name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 
 ]
 
